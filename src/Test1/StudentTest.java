7eb73d11d773a92348da103fa08bead54a387515
lrs <337887790@qq.com>
2021-05-18 22:22:17 +0800

    = =

--- src/Test1/StudentTest.java
+++ src/Test1/StudentTest.java
@@ -8,19 +8,17 @@ package Test1;
 public class StudentTest {
     public static void main(String[] args){
         var lrs = new School[3];
-
 //        School sc = new School().txt();
         lrs[0] = new School("凌瑞松1",23,3);
         lrs[1] = new School("凌瑞松2",23,3);
         lrs[2] = new School("凌瑞松3",23,3);
-        lrs[0].txt();
-
-//        var lbr = new Student[3];
-//        lbr[1].txt1();
-
         for (School s:lrs
-             ) {
+        ) {
             System.out.println("姓名："+s.getName()+",年龄："+s.getAge()+",年级："+s.getGrade());
         }
+        lrs[0].txt();
+
+        Student st = new Student("刘柏汝1",22,4);
+        st.txt1();
     }
 }
